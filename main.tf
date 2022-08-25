8fc5e9749926645ba0f883cb2afb73f733abd1d2
bjr33s <ben.rees@coles.com.au>
2022-08-25 14:59:41 +1000

    updated netowkr

--- main.tf
+++ main.tf
@@ -11,6 +11,13 @@ provider "azurerm" {
   features {}
 }
 
+
+module "network" {
+  source  = "app.terraform.io/bens-hotdog-stand/network/azurerm"
+  version = "3.5.0"
+ resource_group_name = azurerm_resource_group.myresourcegroup.name 
+}
+
 resource "azurerm_resource_group" "myresourcegroup" {
   name     = "${var.prefix}-workshop"
   location = var.location
